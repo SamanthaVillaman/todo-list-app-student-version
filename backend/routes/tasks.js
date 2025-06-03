e203ce9e149c40dae44f903c9fb425f5c2c226e9
SamanthaVillaman <Samanthav127@nycstudents.net>
2025-06-03 14:17:09 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 // There is a bug in line 4 you need to fix it
-const taskModel = require("../model/taskModel");
+const taskModel = require("../models/taskModel");
 
 //Write a comment describing the purpose of this route
 router.get("/", async (req, res) => {
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
 // Write a comment describing the purpose of this route
 router.post("/", async (req, res) => {
   //there is a bug in line 15 you need to fix
-  const { name, description } = req.body;
+  const { title, description } = req.body;
   const task = await taskModel.addTask(title, description);
   res.status(201).json(task);
 });
